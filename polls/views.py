847dbecbfbe70ba59c25034bca4bc5d6d2fffd0c
TerGym <kintumark357@gmail.com>
2025-04-23 13:10:10 +0300

    that was part 5

--- polls/views.py
+++ polls/views.py
@@ -4,26 +4,35 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
+from django.utils import timezone
 
 # Create your views here.
 #create an index view for the polls app with httpresponse
 #import the question model
 #import from django http  http404
 #import get obejct from shortcuts
+#i need to import timezone from django.utils
 
 #create a view to show the latest questions
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
+    #amend this query_set
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return the last five published questions (not including those set to be published in the future)."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 #create a view to show details of a question when selected
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 #create a view to show the results of a question
 class ResultsView(generic.DetailView):
